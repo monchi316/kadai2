f17fe31dc8401a76fcdebf9eb0fa3f9be59c6a02
monchi316 <monchi316@gmail.com>
2020-06-06 15:01:36 +0900

    修正完了

--- JS/main.js
+++ JS/main.js
@@ -4,23 +4,30 @@
   const btn = document.getElementById('btn');
 
   btn.addEventListener('click', () => {
+    
     var fizz = document.getElementById('fizz').value;
     var buzz = document.getElementById('buzz').value;
-    const body = document.querySelector('body');
+    var text = document.getElementById('text');
+
+    if (text.hasChildNodes()) {
+      while (text.firstChild) {
+        text.removeChild(text.firstChild);
+      }
+    } 
 
     if (Number.isInteger(Number(fizz)) && Number.isInteger(Number(buzz))) {
       for (let i = 1; i < 100; i++) {
         if (i % fizz === 0 && i % buzz === 0) {
           const answer = document.createElement('p');
-          body.appendChild(answer);
+          text.appendChild(answer);
           answer.textContent = `FizzBuzz ${i}`;
         } else if (i % fizz === 0) {
           const answer = document.createElement('p');
-          body.appendChild(answer);
+          text.appendChild(answer);
           answer.textContent = `Fizz ${i}`;
         } else if (i % buzz === 0) {
           const answer = document.createElement('p');
-          body.appendChild(answer);
+          text.appendChild(answer);
           answer.textContent = `Buzz ${i}`;
         }
       };
@@ -28,7 +35,7 @@
     } else {
       const message = document.createElement('h2');
       message.textContent = '整数値を入力して下さい';
-      body.appendChild(message);
+      text.appendChild(message);
     }
   });
 }
